0e0bce2e9128fea3c27c9f76ed8c6b9040e0e51e
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-04-06 22:00:54 -0400

    Customer Controller

--- src/com/raj/springdemo/controller/CustomerController.java
+++ src/com/raj/springdemo/controller/CustomerController.java
@@ -1,14 +1,26 @@
 package com.raj.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.raj.springdemo.dao.CustomerDAO;
+import com.raj.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
-	public String listCustomers(){
+	public String listCustomers(Model theModel){
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		theModel.addAttribute("customers", theCustomers);
 		return "list-customers";
 	}
 }
