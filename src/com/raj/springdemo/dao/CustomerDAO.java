63e0f7b2f29127b4481b6458fefcab7802fa4b89
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-04-06 21:48:04 -0400

    Add DAO for Customer objects

---
+++ src/com/raj/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.raj.springdemo.dao;
+
+import java.util.List;
+
+import com.raj.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
---
+++ src/com/raj/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,29 @@
+package com.raj.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.raj.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		Session session = sessionFactory.getCurrentSession();
+		Query<Customer> query = session.createQuery("from Customer", Customer.class);
+		List<Customer> result = query.getResultList();
+		return result;
+	}
+
+}
